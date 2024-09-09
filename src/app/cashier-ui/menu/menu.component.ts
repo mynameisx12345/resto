623d1aa4d2ed2f084d122add2a2d8d3624e3ac6f
mynameisx12345 <positivity.x@gmail.com>
2024-09-09 21:29:42 +0800

    search product

--- src/app/cashier-ui/menu/menu.component.ts
+++ src/app/cashier-ui/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MenuListService } from '../menu-list.service';
 import { take, tap } from 'rxjs';
 import { Orders } from '../menu-list.model';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
@@ -10,16 +11,27 @@ import { Orders } from '../menu-list.model';
 })
 export class MenuComponent implements OnInit{
 
-  constructor(private readonly menuService: MenuListService){}
+  constructor(
+    private readonly menuService: MenuListService,
+    private readonly fb: FormBuilder
+  ){}
   
 
-  selectedCategory = 'Foods';
+  selectedCategory = '';
+  selectedSubCategory = '';
   categories = [
     //{ label: 'Cottages/Tables', icon: 'category' },
     { label: 'Foods', icon: 'dinner_dining' },
     { label: 'Drinks', icon: 'liquor' },
     { label: 'Desserts', icon: 'icecream' },
   ];
+
+  subCategories= [
+    { label: 'Pizza', icon: 'dinner_dining', category: 'Food'},
+    { label: 'Pasta', icon: 'dinner_dining', category: 'Food'},
+    
+    
+  ]
 
   products = [
     {
@@ -28,6 +40,7 @@ export class MenuComponent implements OnInit{
       description: 'Tomato, Mozzarela, Zola, Bell Pepper',
       price: '\u20b1340.00 - 12in \n\n \u20b1450.00 - 16in',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Pizza Staff',
       prices: [{ price: 340, size: '12in' },{ price: 450, size: '16in' }],
     },
@@ -38,6 +51,7 @@ export class MenuComponent implements OnInit{
         'Tomato, Mozzarela, Mushroom, Salami, Black Olive, Bell Pepper',
       price: '\u20b1420.00 - 16in \n\n \u20b1505.00 - 12in',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Pizza Paradiso',
       prices: [{ price: 420, size: '12in' },{ price: 505, size: '16in' }],
 
@@ -49,6 +63,7 @@ export class MenuComponent implements OnInit{
       description: 'Four Cheese',
       price: '\u20b1420.00 - 16in \n\n \u20b1505.00 - 12in',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Calzone 4 Formaggi',
       prices: [{ price: 420, size: '12in' },{ price: 505, size: '16in' }],
 
@@ -59,6 +74,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Food 4',
       prices: [{ price: 420, size: '12in' },{ price: 505, size: '16in' }],
 
@@ -69,6 +85,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Food 5',
       prices: [{ price: 420, size: '12in' },{ price: 505, size: '16in' }],
 
@@ -79,6 +96,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Food 6',
       prices: [{ price: 420, size: '12in' },{ price: 505, size: '16in' }],
 
@@ -89,6 +107,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Food 7',
       prices: [{ price: 420, size: '12in' },{ price: 505, size: '16in' }],
 
@@ -99,6 +118,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '',
       type: 'Foods',
+      subType: 'Pizza',
       name: 'Food 8',
       prices: [{ price: 420, size: '12in' },{ price: 505, size: '16in' }],
 
@@ -109,6 +129,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b140.00',
       type: 'Drinks',
+      subType: '',
       name: 'Four Seasons',
       prices: [{ price: 40, size: '' }],
 
@@ -119,6 +140,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b140.00',
       type: 'Drinks',
+      subType: '',
       name: 'Mango',
       prices: [{ price: 40, size: '' }],
 
@@ -129,6 +151,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b140.00',
       type: 'Drinks',
+      subType: '',
       name: 'Pineapple',
       prices: [{ price: 40, size: '' }],
 
@@ -139,6 +162,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b140.00',
       type: 'Drinks',
+      subType: '',
       name: 'Drinks 12',
       prices: [{ price: 40, size: '' }],
 
@@ -149,6 +173,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b140.00',
       type: 'Drinks',
+      subType: '',
       name: 'Drinks 13',
       prices: [{ price: 40, size: '' }],
 
@@ -159,6 +184,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b140.00',
       type: 'Drinks',
+      subType: '',
       name: 'Drinks 14',
       prices: [{ price: 40, size: '' }],
 
@@ -169,6 +195,7 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b140.00',
       type: 'Drinks',
+      subType: '',
       name: 'Drinks 15',
       prices: [{ price: 40, size: '' }],
 
@@ -179,13 +206,51 @@ export class MenuComponent implements OnInit{
       description: '',
       price: '\u20b1140.00',
       type: 'Desserts',
+      subType: '',
       name: 'Leche Flan',
+      prices: [{ price: 140, size: '' }],
+
+    },
+    {
+      id: '19',
+      img: '../../../assets/images/leche.jpg',
+      description: '',
+      price: '\u20b1140.00',
+      type: 'Foods',
+      subType: 'Pizza',
+      name: 'Pizza Ferie',
+      prices: [{ price: 140, size: '' }],
+
+    },
+    {
+      id: '20',
+      img: '../../../assets/images/leche.jpg',
+      description: '',
+      price: '\u20b1140.00',
+      type: 'Foods',
+      subType: 'Pasta',
+      name: 'Pasta 123',
+      prices: [{ price: 140, size: '' }],
+
+    },
+    {
+      id: '21',
+      img: '../../../assets/images/leche.jpg',
+      description: '',
+      price: '\u20b1140.00',
+      type: 'Foods',
+      subType: 'Others',
+      name: 'Others 123',
       prices: [{ price: 140, size: '' }],
 
     },
   ];
   orders:Orders[] = [];
+  fgMenu:any;
   ngOnInit(): void {
+    this.fgMenu = this.fb.group({
+      searchMenu: ['']
+    })
     this.orders$.pipe(
       take(1),
       tap((orders:Orders[])=>{
@@ -196,7 +261,9 @@ export class MenuComponent implements OnInit{
 
   filteredProducts() {
     return this.products.filter(
-      (product) => product.type === this.selectedCategory
+      (product) => product.type === this.selectedCategory && 
+      (this.selectedSubCategory === '' ||(!!this.selectedSubCategory && this.selectedSubCategory === product.subType) ) &&
+      (this.fgMenu.get('searchMenu').value === '' || (!!this.fgMenu.get('searchMenu').value && product.name.toUpperCase().includes(this.fgMenu.get('searchMenu').value.toUpperCase())))
     );
   }
 
@@ -204,5 +271,10 @@ export class MenuComponent implements OnInit{
 
   addOrder(item:Orders){
    this.menuService.addNewOrders(item);
+  }
+
+  changeCategory(category:any){
+    this.selectedCategory = category.label; 
+    this.selectedSubCategory=''
   }
 }
